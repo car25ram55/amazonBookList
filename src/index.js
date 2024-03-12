b1e86be07fb1e5adb677fd84039b58edc714b63e
camo55 <car25ram@gmail.com>
2024-03-12 01:25:42 -0400

    commit5

--- src/index.js
+++ src/index.js
@@ -5,6 +5,22 @@ function Greeting() {
   return <h2>My First Component</h2>;
 }
 
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <h2>image placeholder</h2>;
+const Title = () => {
+  return <h2>Book Title</h2>;
+};
+const Author = () => <h4>Author</h4>;
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(<Greeting />);
